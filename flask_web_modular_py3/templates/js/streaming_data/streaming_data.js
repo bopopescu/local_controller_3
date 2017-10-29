4b7ce75d74ecd2d00b8e5a8d44796691cfa21388
Glenn Edgar <glenn-edgar@onyxengr.com>
2017-10-28 23:50:20 -0700

    daily commit

--- flask_web_modular_py3/templates/js/streaming_data/streaming_data.js
+++ flask_web_modular_py3/templates/js/streaming_data/streaming_data.js
@@ -5,33 +5,97 @@ var x_start_range = 0
 var x_start_index = 0
 var v_min = Number.MAX_SAFE_INTEGER;
 var v_max = -Number.MAX_SAFE_INTEGER;
-
+var ref_field_index
+var v_min
+var v_max_ref 
 var hh = {}
 
 function change_field_index()
 {
-    alert("change_field_index ")
+    
+    $("#field_index").empty()
+    for( i = 0; i < field_keys.length; i++)
+    {
+        
+        $("#field_index").append($("<option></option>").val(i).html(field_keys[i]));
+    } 
+
+    $("#field_index")[0].selectedIndex = ref_field_index;
+    $("#field_index").selectmenu("refresh")
 }
 function change_time_index()
 {
-    alert("change_time_index")
+    $("#time_slider_1").val(x_start_index*100).slider('refresh');
+    $("#time_slider_2").val(x_start_range*100).slider('refresh');
+    
 }
 function change_vertical_index()
 {
-    alert("change_vertical_index ")
+    
+    v_min_ref = v_min
+    v_max_ref = v_max
+    auto_scale()
+    $("#v_slider_1").prop({
+        min: v_min,
+        max: v_max
+     }).slider("refresh");
+     $("#v_slider_1").val(v_min_ref).slider("refresh")
+      
+    $("#v_slider_2").prop({
+        min: v_min,
+        max: v_max
+     }).slider("refresh");
+     $("#v_slider_2").val(v_max_ref).slider("refresh")
+     v_min = v_min_ref 
+     v_max = v_max_ref
+ }
+ 
+function cancel_field_index()
+{
+     
+     $( "#change_index" ).popup( "close" )
 }
 
 function save_field_index()
 {
-    alert("save_field_index ")
+    ref_field_index = $("#field_index").val()
+    v_min = Number.MAX_SAFE_INTEGER
+    v_max = -Number.MAX_SAFE_INTEGER 
+    x_start_index = .75
+    x_start_range = .25    
+    display_data( ref_field_index )
+    $( "#change_index" ).popup( "close" )
+}
+    
+  
+function cancel_time_index()
+{
+     
+     $( "#change_time_index" ).popup( "close" )
 }
 function save_time_index()
 {
-    alert("save_time_index")
+    
+    x_start_index = ($("#time_slider_1").val()/100);
+    x_start_range = ($("#time_slider_2").val()/100); 
+    display_data( ref_field_index )
+    $( "#change_time_index" ).popup( "close" )
+    
+}    
+
+
+function cancel_vertical_index()
+{
+  
+     $( "#change_vertical_index" ).popup( "close" )
 }
 function save_vertical_index()
 {
-    alert("save_vertical_index ")
+    v_min = $("#v_slider_1").val()
+    v_max = $("#v_slider_2").val()
+     display_data( ref_field_index )
+
+    $( "#change_vertical_index" ).popup( "close" )
 }
 
 
@@ -51,6 +115,7 @@ function prepare_data( )
   }
   draw_array = []
   time_data.reverse()
+  /*
   for( i = 0; i < data_length; i++)
   {
      
@@ -58,11 +123,12 @@ function prepare_data( )
      
      draw_array.push(temp_data)
   }
-  x_start_index = time_data.length -1400
-  x_start_range = time_data.length - x_start_index
+  */
+  x_start_index = .75
+  x_start_range = .25
   v_min = Number.MAX_SAFE_INTEGER;
   v_max = -Number.MAX_SAFE_INTEGER;
-
+  ref_field_index = 0
   
 }
 
@@ -112,12 +178,18 @@ function display_data( index )
 
   draw_array = []
   let field_key_ref = field_keys[index]
-  if( ( x_start_index + x_start_range ) > time_data.length)
+  temp_x = Math.round(time_data.length *x_start_index)
+  temp_x_range = Math.round(x_start_range *time_data.length)
+
+  if( ( temp_x + temp_x_range ) >= time_data.length)
   {
-     x_start_range = time_data.length - x_start_index
+     temp_x_range = time_data.length - temp_x
   } 
-  for( let i = x_start_index; i < x_start_index+ x_start_range; i++)
+  
+ 
+  for( let i = temp_x; i < temp_x+ temp_x_range; i++)
   {
+    
      
      temp_data = [ new Date(time_data[i]["time_stamp"]),time_data[i][field_key_ref] ]
      draw_array.push(temp_data)
@@ -163,17 +235,18 @@ $(document).ready(
  
    display_data(0)
    
-  
-  $("#edit_panel_save").bind("click", save_field_index );
+  $("#cancel_index_changes").bind("click",cancel_field_index);
+  $("#make_index_changes").bind("click", save_field_index );
   $("#change_index").on("popupafteropen", change_field_index );
                           
-  
-  $("#edit_panel_save").bind("click", save_time_index )
-  $("#change_index").on("popupafteropen", change_time_index );
-                          
-  
-  $("#edit_panel_save").bind("click", save_vertical_index )
-  $("  #change_time_index").on("popupafteropen", change_field_index );
+
+  $("#cancel_time_changes").bind("click",cancel_time_index);                          
+  $("#make_time_changes").bind("click", save_time_index )
+  $("#change_time_index").on("popupafteropen", change_time_index );
+                    
+  $("#cancel_vertical_changes").bind("click",cancel_vertical_index);
+  $("#make_vertical_changes").bind("click", save_vertical_index )
+  $("  #change_vertical_index").on("popupafteropen", change_vertical_index );
 
 
   })
