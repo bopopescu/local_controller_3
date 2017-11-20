fd53b61a205a8dda90f94498d7c4dd905d5a6ad3
Glenn Edgar <glenn-edgar@onyxengr.com>
2017-11-20 12:39:32 -0800

    daily commit

--- flask_web_modular_py3/load_linux_controller_data_py3.py
+++ flask_web_modular_py3/load_linux_controller_data_py3.py
@@ -40,10 +40,25 @@ class Load_Linux_Controller_Data(object):
        with open("tmp_file","r") as myfile:
            data = myfile.readlines()
            
-       title_a = "View Python Error Logs"
+       title_a = "View Python Error Entries"
        header_name_a = title_a
        header_a = title_a
        return self.render_template( "view_file_list",title =title_a, header_name= header_name_a, file_list = data ,header = header_a) 
+
+   def list_reboot_data( self ):
+       #os.system("ls /tmp/*.errr > tmp_file")
+       #output = []
+       #with open("tmp_file","r") as myfile:
+       #    data = myfile.readlines())
+       #for i in data:
+       #    output.append("/nLog Data for File: "+i)
+       #    with open(data[i].strip(),"r" ) as my_file:
+       #       output.append( my_file.readlines())
+       #title_a = "View Python Error Data"
+       #header_name_a = title_a
+       #header_a = title_a
+       #return self.render_template( "view_file_list",title =title_a, header_name= header_name_a, file_list = data ,header = header_a) 
+       pass
 
 
    def display_environmental_conditions(self):
@@ -57,3 +72,8 @@ class Load_Linux_Controller_Data(object):
               data[i] = [ item ]
 
        return self.render_template( "linux_system/display_environmental_conditions",data  = data, keys = data.keys() ) 
+       
+   def linux_time_history( self ):
+       # redis db = 12  key =  LINUX_HOUR_LIST_STORE
+       # display time history graph
+       pass
